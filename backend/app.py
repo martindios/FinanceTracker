b649cab15eafa2acea26c03e85ac6ee350e223e7
Martín González Dios <gonzalezdiosmartin@gmail.com>
2025-02-03 19:01:20 +0100

    v1-funcional

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # Configuración de la conexión a PostgreSQL
 DB_HOST = os.getenv('DB_HOST', 'db')
-DB_NAME = os.getenv('DB_NAME', 'testdb')
+DB_NAME = os.getenv('DB_NAME', 'dbfinancialtracker')
 DB_USER = os.getenv('DB_USER', 'testuser')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'testpassword')
 
@@ -20,16 +20,22 @@ def connect_db():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    number = request.form.get('number')
-    text = request.form.get('text')
+    #number = request.form.get('number')
+    #text = request.form.get('text')
 
-    if not number or not text:
-        return jsonify({'error': 'Número y texto son requeridos'}), 400
+    fecha = request.form.get('date')
+    precio = request.form.get('price')
+    descripcion = request.form.get('description')
+
+    if not fecha or not precio or not descripcion:
+        return jsonify({'error': 'Rellena los campos correctamente'}), 400
 
     try:
         conn = connect_db()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO form_data (number, text) VALUES (%s, %s)", (number, text))
+        print(DB_NAME)
+        cursor.execute("INSERT INTO gastos (fecha, precio, descripcion) VALUES (%s, %s, %s)", (fecha, precio, descripcion))
+        #cursor.execute("INSERT INTO form_data (number, text) VALUES (%s, %s)", (number, text))
         conn.commit()
         cursor.close()
         conn.close()
